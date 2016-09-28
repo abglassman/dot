677f84b416831fe2ad973a0883f92c04a01d8f0e
Adam Glassman <abglassman@gmail.com>
2016-09-28 22:20:33 +0000

    Install fzf and oh-my-zsh.

--- install.sh
+++ install.sh
@@ -4,9 +4,18 @@ DOT_HOME="$( cd `dirname "$0"` && pwd)"
 
 source ${DOT_HOME}/vim/setup.sh
 
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 ln -sf ${DOT_HOME}/bin ${HOME}/bin
 ln -sf ${DOT_HOME}/tmux/tmux.conf ${HOME}/.tmux.conf
 ln -sf ${DOT_HOME}/shell/aliases  ${HOME}/.aliases
 ln -sf ${DOT_HOME}/shell/profile  ${HOME}/.profile
 ln -sf ${DOT_HOME}/shell/gopath   ${HOME}/.gopath
 ln -sf ${DOT_HOME}/shell/zshrc    ${HOME}/.zshrc
+
+
+
